bbef984d3a8fc129b2261bb5c9da797962011dc8
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-08 13:05:44 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+		putchar(letter);
+		letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
