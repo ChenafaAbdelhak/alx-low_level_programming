9bf27b3703889f69acd76086560880a1912b535e
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-08 13:42:32 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int letter = 48;
+
+	while (letter <= 57)
+	{
+		putchar(letter);
+		putchar(44);
+		putchar(32);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
