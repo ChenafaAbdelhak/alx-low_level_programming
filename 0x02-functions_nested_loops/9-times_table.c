6b6dfdde9f2719f75a4884b1c075776f1a5d88e9
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-12 18:59:01 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int row, column, result;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			result = row * column;
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+					_putchar(' ');
+			}
+			if (result < 10)
+			{
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
