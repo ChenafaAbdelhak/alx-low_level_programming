c92d8ce626ea1a79bd76a1daea6758f463b73a9f
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-12 19:16:00 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,12 +25,13 @@ void times_table(void)
 			}
 			if (result < 10)
 			{
+				_putchar(' ');
 				_putchar('0' + result);
 			}
 			else
 			{
-				_putchar('0' + (result / 10));
-				_putchar('0' + (result % 10));
+				_putchar('0' + result);
+				_putchar('0' + result);
 			}
 		}
 		_putchar('\n');
