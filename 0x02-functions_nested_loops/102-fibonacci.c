37dc1f6a0d1b8febc4b988a4eac7c29c2e25a438
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-13 06:22:12 -0700

    14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,13 +7,14 @@
 
 int main(void)
 {
-	long n1,n2,tmp=0,i=2;
+	long n1, n2, tmp, i = 2;
 
 	n1 = 1;
 	n2 = 2;
+	tmp = 0;
 	while (i <= 50)
 	{
-		printf("%ld, ",n1+n2);
+		printf("%ld, ", n1 + n2);
 		tmp = n1;
 		n1 = n2;
 		n2 = n2 + n1;
