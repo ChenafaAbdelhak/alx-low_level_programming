56588fa5944acf58c64d990ecde9b9d236f7052f
ChenafaAbdelhak <102294368+ChenafaAbdelhak@users.noreply.github.com>
2023-09-14 00:47:59 +0100

    14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,49 +1,39 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with
- *        1 and 2, separated by a comma followed by a space.
- * Return: Always 0.
+ *main - Entry point
+ *Return: Always 0
  */
 int main(void)
 {
-	int index;
-	unsigned long prev_fib = 0, current_fib = 1, sum;
-	unsigned long prev_fib_half1, prev_fib_half2;
-	unsigned long current_fib_half1, current_fib_half2;
-	unsigned long half1, half2;
+	int fib_a, fib_b, fib_c, fib_b1, fib_b2, fib_c1, fib_c2;
+	fib_b = 1;
+	fib_c = 2;
 
-	for (index = 0; index < 92; index++)
+	printf("%d", fib_b);
+
+	for (fib_a = 1; fib_a < 91; fib_a++)
 	{
-		sum = prev_fib + current_fib;
-		printf("%lu, ", sum);
-		prev_fib = current_fib;
-		current_fib = sum;
+		printf(", %d", fib_c);
+		fib_c = fib_c + fib_b;
+		fib_b = fib_c - fib_b;
 	}
 
-	prev_fib_half1 = prev_fib / 10000000000;
-	current_fib_half1 = current_fib / 10000000000;
-	prev_fib_half2 = prev_fib % 10000000000;
-	current_fib_half2 = current_fib % 10000000000;
+	fib_b1 = fib_b / 1000000000;
+	fib_b2 = fib_b % 1000000000;
+	fib_c1 = fib_c / 1000000000;
+	fib_c2 = fib_c % 1000000000;
 
-	for (index = 93; index < 99; index++)
+	for (fib_a = 92; fib_a < 99; ++fib_a)
 	{
-		half1 = prev_fib_half1 + current_fib_half1;
-		half2 = prev_fib_half2 + current_fib_half2;
-
-		if (prev_fib_half2 + current_fib_half2 > 999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
-		printf("%lu%010lu", half1, half2);
-		if (index != 98)
-			printf(", ");
-		prev_fib_half1 = current_fib_half1;
-		prev_fib_half2 = current_fib_half2;
-		current_fib_half1 = half1;
-		current_fib_half2 = half2;
+		printf(", %d", fib_c1 + (fib_c2 / 1000000000));
+		printf("%d", fib_c2 % 1000000000);
+		fib_c1 = fib_c1 + fib_b1;
+		fib_b1 = fib_c1 - fib_b1;
+		fib_c2 = fib_c2 + fib_b2;
+		fib_b2 = fib_c2 - fib_b2;
 	}
+
 	printf("\n");
 	return (0);
 }
