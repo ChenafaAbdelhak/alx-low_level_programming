6ec4db81b9b25596a2cfa0e5d81cdd7f84156781
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-14 12:12:04 -0700

    4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_numbers - print numbers
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{	
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
 /**
  *  print_most_numbers - print numbers
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i = 48;
 
