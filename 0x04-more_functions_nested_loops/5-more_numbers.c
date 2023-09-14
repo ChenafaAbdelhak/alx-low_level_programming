893b5ff5f6de4875a90bf5b18fdaa70db70d829e
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-14 15:36:21 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,18 +4,18 @@
  */
 void more_numbers(void)
 {
-	char i = '0', j = '0';
+	int i = 0, j = 0;
 
-	while (i <= '1')
+	while (i <= 1)
 	{
 		j = 0;
-		if (i > '0')
-			_putchar(i);
-		while (j <= '9')
+		if (i > 0)
+			_putchar(i + '0');
+		while (j <= 9)
 		{
-			if (i == '1' && j == '5')
+			if (i == 1 && j == 5)
 				break;
-			_putchar(j);
+			_putchar(j + '0');
 			j++;
 		}
 		i++;
