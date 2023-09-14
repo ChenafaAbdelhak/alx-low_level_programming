6db0c871dfee39b5af4219c69e7b6980e0ae70cd
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-14 12:09:04 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_numbers - print numbers
+ *  print_most_numbers - print numbers
  */
 void print_numbers(void)
 {
 	int i = 48;
 
 	while (i <= 57)
-	{
-		putchar(i);
+	{	
+		if (i != '2' && i != '4')
+			putchar(i);
 		i++;
 	}
 	putchar('\n');
