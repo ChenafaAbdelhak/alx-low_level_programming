99b62641af8fad564077658a051419e48dc9c0cd
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-14 17:09:02 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d",i);
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
