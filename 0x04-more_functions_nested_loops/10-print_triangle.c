035c9f9a45cccdaa656e0541fb4888e7b22c3748
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-14 17:49:46 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - tr
+ * @size: size
+ */
+void print_triangle(int size)
+{
+	int i, j, n = size, k;
+
+	if (n == 0 || n < 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= (n - i); j++)
+				_putchar(' ');
+			for (k = 1; k <=i; k++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
