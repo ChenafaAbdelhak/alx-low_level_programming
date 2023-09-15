6ec844b6ac224c94af7f14043b82e61d6c3a18d0
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-14 19:13:56 -0700

    11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - start
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int n, pf;
+
+	n = 612852475143;
+	for (pf = 2; pf <= n; pf++)
+	{
+		if (n % pf == 0)
+		{
+			n /= pf;
+			pf--;
+		}
+	}
+
+	printf("%ld\n", pf);
+	return (0);
+}
