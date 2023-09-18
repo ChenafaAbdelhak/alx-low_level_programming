2aa2b85c10b0e6c03044433daa52c57d490d01d2
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-18 16:07:05 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - print half
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, i;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
+	while (str[i] != '\0')
+		_putchar(str[i]);
+	_putchar('\n');
