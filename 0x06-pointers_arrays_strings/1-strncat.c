bdae7b078eb9666d0e096fe0370b7072ee8b146d
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-21 15:10:43 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - concate n
+ * @dest: dest
+ * @src: source
+ * @n: n
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0, i;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < n  && src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
