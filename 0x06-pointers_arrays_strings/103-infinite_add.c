d4bf21eda036a47c832c02f610472c8465ea5def
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-22 16:36:55 -0700

    11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,46 +1,80 @@
 #include "main.h"
-/**
- * infinite_add - add
- * @n1: n1
- * @n2: n2
- * @r: r
- * @size_r: size
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int len1 = 0, len2 = 0, i = 0, k = 0, res = 0, d1, d2;
 
-	while (n1[len1 + 1] != '\0')
-		len1++;
-	while (n2[len2 + 1] != '\0')
-		len2++;
-	while (i >= len1 && i >= len2)
+/**
+ *rev_string - reverse array
+ *@n: integer params
+ *Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
 	{
-		if (i > len1)
-			d1 = 0;
-		else
-			d1 = n1[i] % 48;
-		if (i > len2)
-			d2 = 0;
-		else
-			d2 = n2[i] % 48;
-		if (len1 > size_r || len2 > size_r || k >= size_r)
-			return (0);
-		res = d1 + d2;
-		if (res < 10)
-		{
-			r[k] = res + 48;
-			k++;
-		}
-		else
-		{
-			r[k] = (res / 10) + 48;
-			k++;
-			r[k] = (res % 10) + 48;
-			k++;
-		}
 		i++;
 	}
-	r[k] = '\0';
+
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ *infinite_add - add 2 numbers together
+ *@n1: text representation of 1st number to add
+ *@n2: text representation of 2nd number to add
+ *@r: pointer to buffer
+ *@size_r: buffer size
+ *Return: pointer to calling function
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
