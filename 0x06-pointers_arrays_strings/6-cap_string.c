714cebd7474694041f518c3d6e40ed47d214615a
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-21 17:41:37 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *cap_string - capitalizes all words in a string
+ *@str: input string
+ *Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int capitalize_next = 1;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\t' || str[i] == '\n' || str[i] == ' ' || str[i] == ',' ||
+			str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+			str[i] == '"' || str[i] == '\'' || str[i] == '(' || str[i] == ')' ||
+			str[i] == '{' || str[i] == '}')
+		{
+			capitalize_next = 1;
+		}
+		else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+			capitalize_next = 0;
+		}
+		else
+		{
+			capitalize_next = 0;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
