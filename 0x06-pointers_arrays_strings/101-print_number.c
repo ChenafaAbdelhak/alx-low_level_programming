c5fe826af414c9ff0bd0f5c1305e23a3a6249521
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-22 14:25:44 -0700

    9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 /**
  * print_number - print
  * @n: int
@@ -7,9 +7,22 @@ i#include "main.h"
 
 void print_number(int n)
 {
+	int len = 1, tmp = n;
+
 	if (n == 0)
 		_putchar('0');
 	if (n < 0)
 		_putchar('-');
-
+	while (tmp !=  0)
+	{
+		tmp = tmp / 10;
+		len *= 10;
+	}
+	while (len > 1)
+	{
+		_putchar((n / len) + 48);
+		if (n - len > 0)
+			n -= (n / len);
+		len /= 10;
+	}
 }
