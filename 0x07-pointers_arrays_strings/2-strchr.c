b8af5b35a4a8ce7ba8815d58815c842dbf407c61
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-25 05:40:08 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - search for a char
+ * @s: the buffer
+ * @c: char
+ * Return: the char position
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char *none = NULL;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (none);
+}
