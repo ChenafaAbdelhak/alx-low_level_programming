2bb95045e6fc0af6d867ab5af9ba7e49bd1d2544
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-09-28 12:24:09 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - count leters;
+ * @s: string
+ * Return: lenth
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+	{
+		s++;
+		return (1 + _strlen_recursion(s));
+	}
+}
