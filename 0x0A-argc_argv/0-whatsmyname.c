41df02fd506b5d9670d00cf751a8d04c19a0f01d
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-02 16:00:24 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @argc: count
+ * @argv: string v
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
