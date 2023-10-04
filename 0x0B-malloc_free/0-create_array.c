13f9912bb27902cfdc1961e4ddfb4a9255bc61fc
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-04 12:07:56 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *str = malloc((sizeof(char) * size));
 	unsigned int i = 0;
 
-	if (str == NULL || size ==0)
+	if (str == NULL || size == 0)
 		return (NULL);
 	while (i < size)
 	{
