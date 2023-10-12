128ab23f4c4d8b3cec9bdfd44a26c2b47e936a23
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-12 15:28:26 -0700

    solve task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print undefined number of integers
+ * @seperator: char to separate printed integers
+ * @n:the number of integers
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+ {
+	unsigned int i;
+	va_list ar;
+	va_start(ar, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ar, int));
+		if (separator)
+			if (i != n - 1)
+				printf("%c", *separator);
+	}
+	printf("\n");
+ }
