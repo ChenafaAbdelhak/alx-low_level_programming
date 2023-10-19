61d35c3ba30eb1b8fcd1cf3b77111e5db61e1737
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-19 16:16:28 -0700

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,23 +7,30 @@
  * Return: new head
  */
 
-
-list_t *add_node(list_t **head, const char *str)
+list_t* add_node(list_t **head, const char *str)
 {
-	list_t node;
+	list_t * new_node;
 	unsigned int len = 0;
 
-	if (head == NULL)
+	while (str[len])
+		len++;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
 		return (NULL);
 
-	node->str = strdup(str);
-	while (*str != NULL)
-	{
-		str++;
-		len++;
-	}
-	node->len = len;
-	node->next = head;
+	new_node->str = strdup(str);
 
-	return (node);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
