a7f9fac3b68d1af953952a8a9b554b08ede7a1ab
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-19 16:52:54 -0700

    3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,14 +3,15 @@
 /**
  * add_node_end - add a node at the end
  * @head: the head
- * str: string
+ * @str: string
+ * Return: head
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *tail, *tmp;
 	unsigned int len = 0;
-	
+
 	while (str[len] != '\0')
 		len++;
 
