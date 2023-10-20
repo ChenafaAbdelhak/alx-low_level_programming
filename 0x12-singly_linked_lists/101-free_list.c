b40c7d13148bb36ee59fd6fface9e77bfb026810
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-19 17:10:35 -0700

    4

---
+++ 0x12-singly_linked_lists/101-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free the list
+ * @head: the list head
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (tmp->next != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+
+	free(tmp);
+}
