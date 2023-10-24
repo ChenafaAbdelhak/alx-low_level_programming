9b46a8e12c7a2fddf4322a42754c555ba81fe569
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-24 16:09:21 -0700

    8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - get the sum node
+ * @head: the head
+ * Return: the sum node
+ */
+
+int sum_listint(listint_t *head)
+{
+	 int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+		i++;
+	}
+	return (sum);
+}
