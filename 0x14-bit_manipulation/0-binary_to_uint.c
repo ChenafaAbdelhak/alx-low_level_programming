8fd0c476be77280ccd97dfb96fd394ce8a5c2d96
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-27 09:54:53 -0700

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - b to int
+ * @b: binary string
+ * Return: integer
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, len = 0;
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[len] != '\0')
+		len++;
+
+	for (i= 0; i < len; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+			result = (result << 1) | (b[i] + '0');
+		else
+			return (0);
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef BIT_MAN
+#define BIT_MAN
+
+#include <stddef.h>
+
+void _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
