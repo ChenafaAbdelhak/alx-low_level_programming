9ab32721a2be53dfadd88fa33d2fbadc9f424637
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-27 14:08:39 -0700

    4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - set bit to 0 at an index
+ * @n: number
+ * @index: index
+ * Return: status 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask = 1;
+
+	if (index < (sizeof(unsigned int) * 8))
+	{
+		mask = mask << index;
+		*n &= mask;
+	}
+	else
+	{
+		return(-1);
+	}
+}
