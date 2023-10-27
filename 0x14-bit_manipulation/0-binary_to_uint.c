4298cc5566c9942d137caa05052e482f7004d73e
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-27 10:47:37 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,8 +19,8 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; i < len; i++)
 	{
-		if (b[i] == '0' || b[i] == '1')
-			result = (result << 1) | (b[i] + '0');
+		if (b[i] == '1' || b[i] == '0')
+			result = (result << 1) | (b[i] - '0');
 		else
 			return (0);
 	}
