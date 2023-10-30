fd106dcee3c2acc080f8cbe101c1bb384d5a3fbb
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-30 16:19:01 -0700

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - read and print text file
+ * @filename : the filename to start
+ * @letters : num of letters
+ * Return: 
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t res;
+	char buff[1024 * 8];
+
+	if (!filename || !letters)
+		return (0);
+
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+		return (0);
+
+	res = read(f, &buff[0], letters);
+	res = write(STDOUT_FILENO, &buff[0], res);
+	close(f);
+	return (res);
+}
