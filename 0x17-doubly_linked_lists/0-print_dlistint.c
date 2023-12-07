38eaf64bb15fd5a0f9578715e9f0e97a3564a482
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-12-07 05:51:46 -0800

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,14 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
-
+	
+	if (!h)
+		return (i);
 	while (h)
 	{
 		printf("%d\n",h->n);
 		i++;
 		h = h->next;
 	}
-	return i;
+	return (i);
 }
