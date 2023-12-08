878f6daf5be2ab18fb2ad8f1154eac5841e157eb
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-12-07 16:58:08 -0800

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,11 +11,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node, *i_node, *pr, *head = *h;
 
-	if (*h == NULL && idx != 0)
-		return (NULL);
-
 	if (idx == 0)
-		return add_dnodeint(&head, n);
+		return add_dnodeint(h, n);
 
 	node = get_dnodeint_at_index(head, idx);
 	if (!node)
@@ -33,4 +30,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	node->prev = i_node;
 
 	return (i_node);
+}
+
+/**
+ * get_dnodeint_at_index - get the idxth node
+ * @head: the head
+ * @index: index
+ * Return: the idxth node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+        unsigned int i = 0;
+
+        while (head)
+        {
+                if (i == index)
+                        return (head);
+                i++;
+                head = head->next;
+        }
+        return (NULL);
 }
