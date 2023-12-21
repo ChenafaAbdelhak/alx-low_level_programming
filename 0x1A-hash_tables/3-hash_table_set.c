26b3247561349f7e84309fdbec474fd4f62dc713
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-12-21 14:23:20 -0800

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,7 +30,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		node = node->next;
 	}
-
 	node = malloc(sizeof(hash_node_t *));
 	if (!node)
 		return (0);
@@ -44,8 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = strdup(value);
 	if (node->value == NULL)
 	{
-		free(node->key);
-		free(node);
+		free(node->key), free(node);
 		return (0);
 	}
 	node->next = ht->array[idx];
