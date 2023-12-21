052d83e168da656aa33a550c3429ad03c9262e20
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-12-21 12:35:42 -0800

    task 1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function creates a hash table of siz size
+ * @size: the size of hash table (number of items)
+ * Return: a pointer to the hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	hash_node_t **items;
+
+	ht = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	items = (hash_node_t **) calloc(size, sizeof(hash_node_t **));
+	if(!items)
+		return(NULL);
+
+	ht->array = items;
+	return (ht);
+}
